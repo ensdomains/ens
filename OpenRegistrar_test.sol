25788fe71fd20a16a204d284da1a1f8a55dd5f3a
Nick Johnson <arachnid@notdot.net>
2016-06-28 13:20:49 +0100

    Fix tests

--- OpenRegistrar_test.sol
+++ OpenRegistrar_test.sol
@@ -9,19 +9,10 @@ contract OpenRegistrarTest is Test {
 		reg = new OpenRegistrar();
 	}
 
-	function testRegister() {
-		reg.register(sha3("foo"));
-		var (rcode, ttl, rnode, raddress) = reg.findResolver(0, sha3("foo"));
-		assertEq(uint(rcode), 0);
-		assertEq(uint(ttl), 3600);
-		assertEq(PersonalResolver(raddress).owner(), address(this));
-		assertEq(uint(rnode), 0);
-		assertEq(reg.getOwner(sha3("foo")), address(this));
-	}
-
 	function testThrowOnExistingDomain() {
-		reg.register(sha3("foo"));
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
+		reg.register(sha3("foo"), pr, 0);
 	}
 
 	function testRegisterWithResolver() {
@@ -36,41 +27,47 @@ contract OpenRegistrarTest is Test {
 	}
 
 	function testTransfer() {
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
 		reg.setOwner(sha3("foo"), address(reg));
 		assertEq(reg.getOwner(sha3("foo")), address(reg));
 	}
 
 	function testThrowOnTransferFromNonOwner() {
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
 		reg.setOwner(sha3("foo"), address(reg));
 		reg.setOwner(sha3("foo"), address(this));
 	}
 
 	function testThrowOnTransferToZero() {
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
 		reg.setOwner(sha3("foo"), address(0));
 	}
 
 	function testSetResolver() {
-		reg.register(sha3("foo"));
 		var pr = new PersonalResolver(address(this));
-		reg.setResolver(sha3("foo"), pr, 0);
+		reg.register(sha3("foo"), pr, 0);
+		var pr2 = new PersonalResolver(address(this));
+		reg.setResolver(sha3("foo"), pr2, 0);
 		var (rcode, ttl, rnode, raddress) = reg.findResolver(0, sha3("foo"));
 		assertEq(uint(rcode), 0);
 		assertEq(uint(ttl), 3600);
-		assertEq(raddress, address(pr));
+		assertEq(raddress, address(pr2));
 		assertEq(uint(rnode), 0);
 	}
 
 	function testThrowOnSetResolverFromNonOwner() {
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
 		reg.setOwner(sha3("foo"), address(reg));
 		reg.setResolver(sha3("foo"), address(this), 0);
 	}
 
 	function testThrowOnSetResolverToZero() {
-		reg.register(sha3("foo"));
+		var pr = new PersonalResolver(address(this));
+		reg.register(sha3("foo"), pr, 0);
 		reg.setResolver(sha3("foo"), address(0), 0);
 	}
 }
