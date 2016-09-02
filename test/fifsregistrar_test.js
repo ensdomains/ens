0ea36dc760a768a51f63ac2601d6578fb580eaef
Nick Johnson <arachnid@notdot.net>
2016-09-02 17:05:06 +0100

    Rewrote tests in JS

---
+++ test/fifsregistrar_test.js
@@ -0,0 +1,103 @@
+var assert = require('assert');
+var async = require('async');
+
+var utils = require('./utils.js');
+var web3 = utils.web3;
+
+var accounts = null;
+
+before(function(done) {
+	web3.eth.getAccounts(function(err, acct) {
+		accounts = acct
+		done();
+	});
+});
+
+describe('FIFSRegistrar', function() {
+	var registrarCode = null;
+	var registrar = null;
+	var ens = null;
+
+
+	before(function() {
+		this.timeout(10000);
+		registrarCode = utils.compileContract(['ENS.sol', 'FIFSRegistrar.sol'], 'FIFSRegistrar');
+	});
+
+	beforeEach(function(done) {
+		async.series([
+			function(done) { ens = utils.deployENS(accounts[0], done); },
+			function(done) {
+				registrar = web3.eth.contract(JSON.parse(registrarCode.interface)).new(
+				    ens.address,
+				    0,
+				    {
+				    	from: accounts[0],
+				     	data: registrarCode.bytecode,
+				     	gas: 4700000
+				   	}, function(err, contract) {
+				   		if(contract.address != undefined)
+				   	    	ens.setOwner(0, registrar.address, {from: accounts[0]}, done);
+				   	});
+			}],
+			done
+		);
+	});
+
+	it('registers names', function(done) {
+		async.series([
+			function(done) {
+				registrar.register(web3.sha3('eth'), accounts[0], {from: accounts[0]}, done);
+			},
+			function(done) {
+				ens.owner(0, function(err, address) {
+					assert.equal(err, null, err);
+					assert.equal(address, registrar.address);
+					done();
+				});
+			},
+			function(done) {
+				ens.owner(utils.node, function(err, address) {
+					assert.equal(err, null, err);
+					assert.equal(address, accounts[0]);
+					done();
+				});
+			}],
+			done
+		);
+	});
+
+	it('transfers names', function(done) {
+		async.series([
+			function(done) {
+				registrar.register(web3.sha3('eth'), accounts[0], {from: accounts[0]}, done);
+			},
+			function(done) {
+				registrar.register(web3.sha3('eth'), accounts[1], {from: accounts[0]}, done);
+			},
+			function(done) {
+				ens.owner(utils.node, function(err, address) {
+					assert.equal(err, null, err);
+					assert.equal(address, accounts[1]);
+					done();
+				});
+			}],
+			done
+		);		
+	});
+
+	it('forbids transferring names you do not own', function(done) {
+		async.series([
+			function(done) {
+				registrar.register(web3.sha3('eth'), accounts[1], {from: accounts[0]}, done);
+			},
+			function(done) {
+				registrar.register(web3.sha3('eth'), accounts[0], {from: accounts[0]}, function(err, txid) {
+					assert.ok(err.toString().indexOf(utils.INVALID_JUMP) != -1, err);
+					done();
+				});
+			}],
+			done
+		);
+	});
+});
---
+++ test/publicresolver_test.js
@@ -0,0 +1,90 @@
+var assert = require('assert');
+var async = require('async');
+
+var utils = require('./utils.js');
+var web3 = utils.web3;
+
+var accounts = null;
+
+before(function(done) {
+	web3.eth.getAccounts(function(err, acct) {
+		accounts = acct
+		done();
+	});
+});
+
+describe('PublicResolver', function() {
+	var resolverCode = null;
+	var resolver = null;
+	var ens = null;
+
+
+	before(function() {
+		this.timeout(10000);
+		resolverCode = utils.compileContract(['ENS.sol', 'PublicResolver.sol'], 'PublicResolver');
+	});
+
+	beforeEach(function(done) {
+		async.series([
+			function(done) { ens = utils.deployENS(accounts[0], done); },
+			function(done) {
+				resolver = web3.eth.contract(JSON.parse(resolverCode.interface)).new(
+				    ens.address,
+				    {
+				    	from: accounts[0],
+				     	data: resolverCode.bytecode,
+				     	gas: 4700000
+				   	}, function(err, contract) {
+				   	    assert.equal(err, null, err);
+				   	    if(contract.address != undefined) {
+				   	    	ens.setSubnodeOwner(0, web3.sha3('eth'), accounts[0], {from: accounts[0]}, done);
+					   	}
+				   });
+			}],
+			done
+		);
+	});
+
+	it('permits setting addresses', function(done) {
+		async.series([
+			function(done) { resolver.has(utils.node, "addr", function(err, result) {
+					assert.equal(err, null, err);
+					assert.equal(result, false);
+					done();
+				});
+			},
+			function(done) { resolver.setAddr(utils.node, accounts[1], {from: accounts[0]}, function(err, txid) {
+					assert.equal(err, null, err);
+					done();
+				});
+			},
+			function(done) { resolver.addr(utils.node, function(err, address) {
+					assert.equal(err, null, err);
+					assert.equal(address, accounts[1]);
+					done();
+				});
+			},
+			function(done) { resolver.has(utils.node, "addr", function(err, result) {
+					assert.equal(err, null, err);
+					assert.equal(result, true);
+					done();
+				});
+			}],
+			done
+		);
+	});
+
+	it('forbids setting addresses by non-owners', function(done) {
+		resolver.setAddr(utils.node, accounts[1], {from: accounts[1]}, function(err, tx) {
+			assert.ok(err.toString().indexOf(utils.INVALID_JUMP) != -1, err);
+			done();
+		});
+	});
+
+	it('throws when fetching nonexistent addresses', function(done) {
+		resolver.addr(utils.node, function(err, result) {
+			assert.ok(err.toString().indexOf(utils.INVALID_JUMP) != -1, err);
+			done();
+		});
+	});
+});
