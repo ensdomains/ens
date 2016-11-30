2bd60471cbfbb0a07c9bbfb1d5f2f8b248719f8f
Daniel Ellison <daniel@syrinx.net>
2016-11-30 10:13:51 -0500

    Added first pass at an LLL implementation of the public resolver.

---
+++ public-resolver.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+
+contract AbstractPublicResolver {
+    function PublicResolver(address ensAddr);
+    function has(bytes32 node, bytes32 kind) constant returns (bool);
+    function supportsInterface(bytes4 interfaceID) constant returns (bool);
+    function addr(bytes32 node) constant returns (address ret);
+    function setAddr(bytes32 node, address addr);
+    function hash(bytes32 node) constant returns (bytes32 ret);
+    function setHash(bytes32 node, bytes32 hash);
+}
