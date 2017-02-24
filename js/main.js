170f466e2dcee1d72d0498aa9266b49cc625fed2
Nick Johnson <arachnid@notdot.net>
2017-02-24 14:03:14 +0000

    Remove leftover google maps gumph

--- js/main.js
+++ js/main.js
@@ -122,24 +122,4 @@
             });
 
         }
-    
-    //Google Map
-    var get_latitude = $('#google-map').data('latitude');
-    var get_longitude = $('#google-map').data('longitude');
-
-    function initialize_google_map() {
-        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-        var mapOptions = {
-            zoom: 14,
-            scrollwheel: false,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize_google_map);
-
 })(jQuery);
