88db2800bf11104523921658ec01d5c52f08e190
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-03-26 21:53:48 +0100

    Promisified ReverseRegistrar tests.

--- test/reverseregistrar_test.js
+++ test/reverseregistrar_test.js
@@ -1,20 +1,23 @@
 var assert = require('assert');
 var async = require('async');
+var Promise = require('bluebird');
 
 var utils = require('./utils.js');
+Promise.promisifyAll(utils);
 var web3 = utils.web3;
+Promise.promisifyAll(web3.eth);
 
 var accounts = null;
 var node = null;
 
-before(function(done) {
-    web3.eth.getAccounts(function(err, acct) {
-        accounts = acct
-        node = web3.sha3('0000000000000000000000000000000000000000000000000000000000000000'
-                         + web3.sha3(accounts[0].slice(2).toLowerCase()).slice(2),
-                         {encoding: 'hex'});
-        done();
-    });
+before(function() {
+    return web3.eth.getAccountsAsync()
+        .then(function(acct) {
+            accounts = acct
+            node = web3.sha3('0000000000000000000000000000000000000000000000000000000000000000'
+                             + web3.sha3(accounts[0].slice(2).toLowerCase()).slice(2),
+                             {encoding: 'hex'});
+        });
 });
 
 describe('ReverseRegistrar', function() {
@@ -28,47 +31,51 @@ describe('ReverseRegistrar', function() {
         registrarCode = utils.compileContract(['interface.sol', 'ReverseRegistrar.sol']).contracts['ReverseRegistrar.sol:ReverseRegistrar'];
     });
 
-    beforeEach(function(done) {
-        async.series([
-            function(done) { ens = utils.deployENS(accounts[0], done); },
-            function(done) {
-                registrar = web3.eth.contract(JSON.parse(registrarCode.interface)).new(
-                    ens.address,
-                    0,
-                    {
-                        from: accounts[0],
-                        data: registrarCode.bytecode,
-                        gas: 4700000
-                    }, function(err, contract) {
-                        if(contract.address != undefined)
-                            ens.setOwner(0, registrar.address, {from: accounts[0]}, done);
-                    });
-            }],
-            done
-        );
-    });
-
-    it('calculates node hashes correctly', function(done) {
-        registrar.node(accounts[0], function(err, hash) {
-            assert.equal(err, null, err);
-            assert.equal(hash, node);
-            done();
-        });
-    });
-
-    it('allows an account to claim its address', function(done) {
-        async.series([
-            function(done) {
-                registrar.claim(accounts[1], {from: accounts[0]}, done);
-            },
-            function(done) {
-                ens.owner(node, function(err, result) {
-                    assert.equal(err, null, err);
-                    assert.equal(result, accounts[1]);
-                    done();
+    beforeEach(function() {
+        this.timeout(4000);
+        return utils.deployENSAsync(accounts[0])
+            .then(function(_ens) {
+                ens = _ens;
+                return new Promise(function(resolve, reject) {
+                    web3.eth.contract(JSON.parse(registrarCode.interface)).new(
+                        ens.address,
+                        0,
+                        {
+                            from: accounts[0],
+                            data: registrarCode.bytecode,
+                            gas: 4700000
+                        },
+                        function(err, contract) {
+                            if (err) {
+                                reject(err);
+                            } else if (typeof contract.address !== "undefined") {
+                                resolve(contract);
+                            }
+                        });
                 });
-            }],
-            done
-        );
-    })
+            })
+            .then(function(contract) {
+                registrar = Promise.promisifyAll(contract);
+                return ens.setOwnerAsync(0, registrar.address, {from: accounts[0]});
+            });
+    });
+
+    it('calculates node hashes correctly', function() {
+        this.slow(150);
+        return registrar.nodeAsync(accounts[0])
+            .then(function(hash) {
+                assert.equal(hash, node);
+            });
+    });
+
+    it('allows an account to claim its address', function() {
+        this.slow(150);
+        return registrar.claimAsync(accounts[1], {from: accounts[0]})
+            .then(function(txHash) {
+                return ens.ownerAsync(node);
+            })
+            .then(function(result) {
+                assert.equal(result, accounts[1]);
+            });
+    });
 });
