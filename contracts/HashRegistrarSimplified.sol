f3ca8ad6e3c2b3f2d5e6575b8d26ba7360e33af5
Alex Van de Sande <avsa@ethereum.org>
2017-04-26 13:28:12 -0300

    Make the launch length public (#139)
    building the app, I miss having this information queriable as it varies according to testnet/mainet

--- contracts/HashRegistrarSimplified.sol
+++ contracts/HashRegistrarSimplified.sol
@@ -107,7 +107,7 @@ contract Registrar {
 
     uint32 constant totalAuctionLength = 5 days;
     uint32 constant revealPeriod = 48 hours;
-    uint32 constant launchLength = 13 weeks;
+    uint32 public constant launchLength = 13 weeks;
     uint constant minPrice = 0.01 ether;
     uint public registryStarted;
 
