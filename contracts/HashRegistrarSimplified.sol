667aa3e11694d52f17a5f97f50618911e311a574
ethers <ethereum@outlook.com>
2017-04-29 00:33:03 -0700

    Explain indirect selfdestruct (#148)
    Explanation of why the code is not simply `selfdestruct(owner)` https://github.com/ethereum/ens/issues/124

--- contracts/HashRegistrarSimplified.sol
+++ contracts/HashRegistrarSimplified.sol
@@ -86,6 +86,10 @@ contract Deed {
      */
     function destroyDeed() {
         if (active) throw;
+        
+        // Instead of selfdestruct(owner), invoke owner fallback function to allow
+        // owner to log an event if desired; but owner should also be aware that
+        // its fallback function can also be invoked by setBalance
         if(owner.send(this.balance)) {
             selfdestruct(burn);
         }
