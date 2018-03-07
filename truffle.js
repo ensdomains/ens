b93ba8b371a646abdbdeecdf4c9f362828ae76bc
decanus <dean.eigenmann@icloud.com>
2018-03-07 09:28:55 +0100

    Fixed

--- truffle.js
+++ truffle.js
@@ -1,20 +1,14 @@
-// var TestRPC = require('ethereumjs-testrpc');
-
 module.exports = {
-  // networks: {
-  //   'dev.fifs': {
-  //     host: "localhost",
-  //     port: 8545,
-  //     network_id: "*" // Match any network id
-  //   },
-  //   'dev.auction': {
-  //     host: "localhost",
-  //     port: 8545,
-  //     network_id: "*" // Match any network id
-  //   },
-  //   test: {
-  //     provider: TestRPC.provider(), // in-memory TestRPC provider
-  //     network_id: "*" // Match any network id
-  //   }
-  // }
+  networks: {
+    'dev.fifs': {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    'dev.auction': {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
