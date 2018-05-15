2a1ae54a2164fa29f73dd0fee584e31426742823
Suhail Doshi <digitalwarfare@gmail.com>
2018-05-14 20:45:34 -0700

    - updated FIFSRegistrar to the right abi based on the current .sol contract
    note: the older abi had expiryTimes in it which let you call the function and it returned 0 making you think the domain wasn't registered when in reality the interface doesn't implement it.

--- ensutils.js
+++ ensutils.js
@@ -900,80 +900,39 @@ var deedContract = web3.eth.contract([
 ]);
 
 var fifsRegistrarContract = web3.eth.contract([
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "ens",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "",
-        "type": "bytes32"
-      }
-    ],
-    "name": "expiryTimes",
-    "outputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "payable": false,
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "subnode",
-        "type": "bytes32"
-      },
-      {
-        "name": "owner",
-        "type": "address"
-      }
-    ],
-    "name": "register",
-    "outputs": [],
-    "payable": false,
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "rootNode",
-    "outputs": [
-      {
-        "name": "",
-        "type": "bytes32"
-      }
-    ],
-    "payable": false,
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "name": "ensAddr",
-        "type": "address"
-      },
-      {
-        "name": "node",
-        "type": "bytes32"
-      }
-    ],
-    "type": "constructor"
-  }
+    {
+        constant: false,
+        inputs: [
+            {
+                name: "subnode",
+                type: "bytes32"
+            },
+            {
+                name: "owner",
+                type: "address"
+            }
+        ],
+        name: "register",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function"
+    },
+    {
+        inputs: [
+            {
+                name: "ensAddr",
+                type: "address"
+            },
+            {
+                name: "node",
+                type: "bytes32"
+            }
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "constructor"
+    }
 ]);
 
 var resolverContract = web3.eth.contract([
