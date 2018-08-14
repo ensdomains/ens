8fdf5d3f1e65a148b74f16e196fcb69892656bb3
JosefJ <jelacic.josef@gmail.com>
2018-08-14 17:30:32 +0200

    renamed constructors added event emits

--- contracts/ReverseRegistrar.sol
+++ contracts/ReverseRegistrar.sol
@@ -18,7 +18,7 @@ contract ReverseRegistrar {
      * @param ensAddr The address of the ENS registry.
      * @param resolverAddr The address of the default reverse resolver.
      */
-    function ReverseRegistrar(ENS ensAddr, Resolver resolverAddr) public {
+    constructor(ENS ensAddr, Resolver resolverAddr) public {
         ens = ensAddr;
         defaultResolver = resolverAddr;
 
