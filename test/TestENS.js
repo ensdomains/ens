663c2d20771becac1788e2db9bd495fcf363f905
decanus <dean.eigenmann@icloud.com>
2018-08-24 09:55:01 +0200

    made tests work for LLL

--- test/TestENS.js
+++ test/TestENS.js
@@ -1,99 +1,104 @@
-const ENS = artifacts.require('ENSRegistry.sol');
-
 const utils = require('./helpers/Utils.js');
 const web3Utils = require('web3-utils');
 const namehash = require('eth-ens-namehash');
 
-contract('ENS', function (accounts) {
+let contracts = [
+    [artifacts.require('ENSRegistry.sol'), 'Solidity'],
+    [artifacts.require('ENS.lll'), 'LLL']
+];
 
-    let ens;
+contracts.forEach(function ([ENS, lang]) {
+    contract('ENS ' + lang, function (accounts) {
 
-    beforeEach(async () => {
-        ens = await ENS.new();
-    });
+        let ens;
 
-    it('should allow ownership transfers', async () => {
-        let result = await ens.setOwner(0, '0x1234', {from: accounts[0]});
+        beforeEach(async () => {
+            ens = await ENS.new();
+        });
 
-        assert.equal(await ens.owner(0), '0x0000000000000000000000000000000000001234')
+        it('should allow ownership transfers', async () => {
+            let result = await ens.setOwner(0, '0x1234', {from: accounts[0]});
 
-        assert.equal(result.logs.length, 1);
-        let args = result.logs[0].args;
-        assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
-        assert.equal(args.owner, "0x0000000000000000000000000000000000001234");
-    });
+            assert.equal(await ens.owner(0), '0x0000000000000000000000000000000000001234')
 
-    it('should prohibit transfers by non-owners', async () => {
-        try {
-            await ens.setOwner(1, '0x1234', {from: accounts[0]});
-        } catch (error) {
-            return utils.ensureException(error);
-        }
+            assert.equal(result.logs.length, 1);
+            let args = result.logs[0].args;
+            assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
+            assert.equal(args.owner, "0x0000000000000000000000000000000000001234");
+        });
 
-        assert.fail('transfer did not fail');
-    });
+        it('should prohibit transfers by non-owners', async () => {
+            try {
+                await ens.setOwner(1, '0x1234', {from: accounts[0]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
 
-    it('should allow setting resolvers', async () => {
-        let result = await ens.setResolver(0, '0x1234', {from: accounts[0]});
+            assert.fail('transfer did not fail');
+        });
 
-        assert.equal(await ens.resolver(0), "0x0000000000000000000000000000000000001234");
+        it('should allow setting resolvers', async () => {
+            let result = await ens.setResolver(0, '0x1234', {from: accounts[0]});
 
-        assert.equal(result.logs.length, 1);
-        let args = result.logs[0].args;
-        assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
-        assert.equal(args.resolver, "0x0000000000000000000000000000000000001234");
-    });
+            assert.equal(await ens.resolver(0), "0x0000000000000000000000000000000000001234");
 
-    it('should prevent setting resolvers by non-owners', async () => {
-        try {
-            await ens.setResolver(1, '0x1234', {from: accounts[0]});
-        } catch (error) {
-            return utils.ensureException(error);
-        }
+            assert.equal(result.logs.length, 1);
+            let args = result.logs[0].args;
+            assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
+            assert.equal(args.resolver, "0x0000000000000000000000000000000000001234");
+        });
 
-        assert.fail('setting resolver did not fail');
-    });
+        it('should prevent setting resolvers by non-owners', async () => {
+            try {
+                await ens.setResolver(1, '0x1234', {from: accounts[0]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
 
-    it('should allow setting the TTL', async () => {
-        let result = await ens.setTTL(0, 3600, {from: accounts[0]});
+            assert.fail('setting resolver did not fail');
+        });
 
-        assert.equal((await ens.ttl(0)).toNumber(), 3600);
+        it('should allow setting the TTL', async () => {
+            let result = await ens.setTTL(0, 3600, {from: accounts[0]});
 
-        assert.equal(result.logs.length, 1);
-        let args = result.logs[0].args;
-        assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
-        assert.equal(args.ttl.toNumber(), 3600);
-    });
+            assert.equal((await ens.ttl(0)).toNumber(), 3600);
 
-    it('should prevent setting the TTL by non-owners', async () => {
-        try {
-            await ens.setTTL(1, 3600, {from: accounts[0]});
-        } catch (error) {
-            return utils.ensureException(error);
-        }
+            assert.equal(result.logs.length, 1);
+            let args = result.logs[0].args;
+            assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
+            assert.equal(args.ttl.toNumber(), 3600);
+        });
 
-        assert.fail('setting resolver did not fail');
-    });
+        it('should prevent setting the TTL by non-owners', async () => {
+            try {
+                await ens.setTTL(1, 3600, {from: accounts[0]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
 
-    it('should allow the creation of subnodes', async () => {
-        let result = await ens.setSubnodeOwner(0, web3Utils.sha3('eth'), accounts[1], {from: accounts[0]});
+            assert.fail('setting resolver did not fail');
+        });
 
-        assert.equal(await ens.owner(namehash('eth')), accounts[1]);
+        it('should allow the creation of subnodes', async () => {
+            let result = await ens.setSubnodeOwner(0, web3Utils.sha3('eth'), accounts[1], {from: accounts[0]});
 
-        assert.equal(result.logs.length, 1);
-        let args = result.logs[0].args;
-        assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
-        assert.equal(args.label, web3.sha3('eth'));
-        assert.equal(args.owner, accounts[1]);
-    });
+            assert.equal(await ens.owner(namehash('eth')), accounts[1]);
 
-    it('should prohibit subnode creation by non-owners', async () => {
-        try {
-            await ens.setSubnodeOwner(0, web3Utils.sha3('eth'), accounts[1], {from: accounts[1]});
-        } catch (error) {
-            return utils.ensureException(error);
-        }
+            assert.equal(result.logs.length, 1);
+            let args = result.logs[0].args;
+            assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
+            assert.equal(args.label, web3.sha3('eth'));
+            assert.equal(args.owner, accounts[1]);
+        });
 
-        assert.fail('setting resolver did not fail');
+        it('should prohibit subnode creation by non-owners', async () => {
+            try {
+                await ens.setSubnodeOwner(0, web3Utils.sha3('eth'), accounts[1], {from: accounts[1]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+
+            assert.fail('setting resolver did not fail');
+        });
     });
 });
