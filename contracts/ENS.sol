72baf804ce3d829356079e9e982bc4ec2e47ee0c
Nick Johnson <arachnid@notdot.net>
2020-01-27 10:43:33 +1300

    setSubnodeOwner retrns bytes32

--- contracts/ENS.sol
+++ contracts/ENS.sol
@@ -19,7 +19,7 @@ interface ENS {
 
     function setRecord(bytes32 node, address owner, address resolver, uint64 ttl) external;
     function setSubnodeRecord(bytes32 node, bytes32 label, address owner, address resolver, uint64 ttl) external;
-    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external;
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external returns(bytes32);
     function setResolver(bytes32 node, address resolver) external;
     function setOwner(bytes32 node, address owner) external;
     function setTTL(bytes32 node, uint64 ttl) external;
